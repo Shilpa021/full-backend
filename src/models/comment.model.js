9743665eeebc40f556da74af3478c702a4f660a0
Shilpa021 <rana.shilpa021@gmail.com>
2026-03-09 17:09:01 +0530

    Add models for comments, likes, playlists, and tweets with appropriate schemas

---
+++ src/models/comment.model.js
@@ -0,0 +1,14 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new Schema({
+    content: { type: String, required: true },
+    video: { type: mongoose.Schema.Types.ObjectId, ref: "Video" },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+}, { timestamps: true });
+
+
+//aggregation pipeline
+commentSchema.plugin(mongooseAggregatePaginate);
+
+export const Comment = mongoose.model("Comment", commentSchema);
---
+++ src/models/tweet.model.js
@@ -0,0 +1,8 @@
+import mongoose, { Schema } from "mongoose";
+
+const tweetSchema = new Schema({
+    content: { type: String, required: true, trim: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+}, { timestamps: true });
+
+export const Tweet = mongoose.model("Tweet", tweetSchema);
